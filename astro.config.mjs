50015b426441fbb00d1b6eb594123aeb07864323
Jake <jake@jakefixestech.com>
2026-04-26 21:01:07 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://coldbrookrecovery.co',
   compressHTML: true,
+
   build: {
     inlineStylesheets: 'always'
-  }
+  },
+
+  integrations: [sitemap()]
 });
